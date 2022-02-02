64019769f8dc865605ce4acd1f842e3a39d7bcfa
sungminner <sung.minner@gmail.com>
2022-02-02 22:14:47 +0900

    #10 Studio UI

--- src/components/Playbar.js
+++ src/components/Playbar.js
@@ -14,22 +14,24 @@ const Playbar = ({ currentPage }) => {
   return (
     <>
       <div className="playbar-padding" />
-      <div className="playbar-wrapper">
-        <div className="playbar">
-          <div className="playbar-playlist">
-            <FontAwesomeIcon icon="bars" />
-          </div>
-          <div className="playbar-text">
-            <p className="playbar-title">나의 어깨에 기대어요 {cnt}</p>
-            <p className="playbar-member">sung.minner, ajsdfl1232</p>
-          </div>
-          <div className="playbar-control">
-            <FontAwesomeIcon icon="step-backward" className="playbar-prev" />
-            <FontAwesomeIcon icon="play" className="playbar-play" />
-            <FontAwesomeIcon icon="step-forward" className="playbar-next" />
+      <Link to="/song">
+        <div className="playbar-wrapper">
+          <div className="playbar">
+            <div className="playbar-playlist">
+              <FontAwesomeIcon icon="bars" />
+            </div>
+            <div className="playbar-songinfo">
+              <p className="playbar-title">나의 어깨에 기대어요 {cnt}</p>
+              <p className="playbar-member">sung.minner, ajsdfl1232</p>
+            </div>
+            <div className="playbar-control">
+              <FontAwesomeIcon icon="step-backward" className="playbar-prev" />
+              <FontAwesomeIcon icon="play" className="playbar-play" />
+              <FontAwesomeIcon icon="step-forward" className="playbar-next" />
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
