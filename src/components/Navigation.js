f80d583a60b6dbfeec4737a1d4af3a3078345327
sungminner <sung.minner@gmail.com>
2022-02-07 12:56:30 +0900

    #15 Hide nav & playbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "css/navigation.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import usePathname from "functions/usePathname";
+import "css/navigation.css";
 
 const Navigation = () => {
+  const pathname = usePathname();
   return (
     <>
-      <div className="nav__padding" />
-      <div className="nav__wrapper">
-        <nav className="nav">
-          <div className="nav__link">
-            <Link to="/explore">
-              <FontAwesomeIcon
-                icon="globe-americas"
-                className="nav__link-icon"
-              />
-              <p className="nav__link-detail enReg">Explore</p>
-            </Link>
-          </div>
-          <div className="nav__link">
-            <Link to="/">
-              <FontAwesomeIcon icon="home" className="nav__link-icon" />
-              <p className="nav__link-detail enReg">Home</p>
-            </Link>
-          </div>
-          <div className="nav__link">
-            {/* {userObj ? ( */}
-            <Link to="/my">
-              <FontAwesomeIcon icon="user" className="nav__link-icon" />
-              <p className="nav__link-detail enReg">My</p>
-            </Link>
-            {/* ) : (
+      {pathname === "Song" || pathname === "Studio" ? (
+        <></>
+      ) : (
+        <>
+          <div className="nav__padding" />
+          <div className="nav__wrapper">
+            <nav className="nav">
+              <div className="nav__link">
+                <Link to="/explore">
+                  <FontAwesomeIcon
+                    icon="globe-americas"
+                    className="nav__link-icon"
+                  />
+                  <p className="nav__link-detail enReg">Explore</p>
+                </Link>
+              </div>
+              <div className="nav__link">
+                <Link to="/">
+                  <FontAwesomeIcon icon="home" className="nav__link-icon" />
+                  <p className="nav__link-detail enReg">Home</p>
+                </Link>
+              </div>
+              <div className="nav__link">
+                {/* {userObj ? ( */}
+                <Link to="/my">
+                  <FontAwesomeIcon icon="user" className="nav__link-icon" />
+                  <p className="nav__link-detail enReg">My</p>
+                </Link>
+                {/* ) : (
             <Link to="/auth">
               <FontAwesomeIcon icon="user" className="nav__link-icon" />
               <p className="nav__link-detail">My</p>
             </Link>
           )} */}
+              </div>
+            </nav>
           </div>
-        </nav>
-      </div>
+        </>
+      )}
     </>
   );
 };
--- src/components/Playbar.js
+++ src/components/Playbar.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "css/playbar.css";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import usePathname from "functions/usePathname";
+import "css/playbar.css";
 
-const Playbar = ({ currentPage }) => {
+const Playbar = () => {
+  const pathname = usePathname();
   const [cnt, setCnt] = useState(0);
   useEffect(() => {
     const timer = setInterval(() => {
@@ -13,25 +15,37 @@ const Playbar = ({ currentPage }) => {
   });
   return (
     <>
-      <div className="playbar-padding" />
-      <Link to="/song">
-        <div className="playbar-wrapper">
-          <div className="playbar">
-            <div className="playbar-playlist">
-              <FontAwesomeIcon icon="bars" />
+      {pathname === "Song" || pathname === "Studio" ? (
+        <></>
+      ) : (
+        <>
+          <div className="playbar-padding" />
+          <Link to="/song">
+            <div className="playbar-wrapper">
+              <div className="playbar">
+                <div className="playbar-playlist">
+                  <FontAwesomeIcon icon="bars" />
+                </div>
+                <div className="playbar-songinfo">
+                  <p className="playbar-title">나의 어깨에 기대어요 {cnt}</p>
+                  <p className="playbar-member">sung.minner, ajsdfl1232</p>
+                </div>
+                <div className="playbar-control">
+                  <FontAwesomeIcon
+                    icon="step-backward"
+                    className="playbar-prev"
+                  />
+                  <FontAwesomeIcon icon="play" className="playbar-play" />
+                  <FontAwesomeIcon
+                    icon="step-forward"
+                    className="playbar-next"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="playbar-songinfo">
-              <p className="playbar-title">나의 어깨에 기대어요 {cnt}</p>
-              <p className="playbar-member">sung.minner, ajsdfl1232</p>
-            </div>
-            <div className="playbar-control">
-              <FontAwesomeIcon icon="step-backward" className="playbar-prev" />
-              <FontAwesomeIcon icon="play" className="playbar-play" />
-              <FontAwesomeIcon icon="step-forward" className="playbar-next" />
-            </div>
-          </div>
-        </div>
-      </Link>
+          </Link>
+        </>
+      )}
     </>
   );
 };
