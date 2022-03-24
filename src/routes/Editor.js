2e3892c0c1c395335f4932f90520489e68f65fa0
sungminner <sung.minner@gmail.com>
2022-03-24 23:40:18 +0900

    71 Simplify play/pause/mute

--- src/routes/Editor.js
+++ src/routes/Editor.js
@@ -6,56 +6,11 @@ import Recorder from "components/Recorder";
 import base_URL from "base_URL";
 import "css/editor.css";
 
-class Track {
-  constructor(outputNode, element) {
-    this._context = outputNode.context;
-    this._element = element;
-    this._outputNode = outputNode;
-    this._source = new MediaElementAudioSourceNode(this._context, {
-      mediaElement: this._element,
-    });
-    this._amp = new GainNode(this._context);
-    this._source.connect(this._amp).connect(this._outputNode);
-    this._muted = false;
-  }
-
-  toggleMute() {
-    if (this._muted) {
-      this._amp.gain.value = 1;
-      this._muted = false;
-    } else {
-      this._amp.gain.value = 0;
-      this._muted = true;
-    }
-  }
-
-  // play() {}
-
-  // pause() {
-  //   console.log(this._context.state);
-  //   // this._element.pause();
-  //   if (this._context.state === "running") {
-  //     this._element.pause();
-  //     // this._context.suspend().then(() => {
-  //     //   console.log("suspended");
-  //     // });
-  //   } else if (this._context.state === "suspended") {
-  //     this._element.play();
-  //     // this._context.resume().then(() => {
-  //     //   console.log("resumed");
-  //     // });
-  //   }
-  // }
-}
-
 const Editor = ({ userObj }) => {
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const audioCtx = new AudioContext();
   const { randomKey, sessionid } = useParams();
   const [songID, setSongID] = useState(null);
   const [songObj, setSongObj] = useState();
   const [sessions, setSessions] = useState();
-  const [tracks, setTracks] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
   const synthesized = useRef(null);
   const sessionsRef = useRef([]);
@@ -80,18 +35,7 @@ const Editor = ({ userObj }) => {
   }, [songID]);
   useEffect(() => {
     sessionsRef.current = sessionsRef.current.slice(0, sessionsRef.length);
-    if (sessionsRef.current.length !== 0) {
-      sessionsRef.current.forEach((element) => {
-        setTracks((prev) => [
-          ...prev,
-          new Track(audioCtx.destination, element),
-        ]);
-      });
-    }
   }, [sessions]);
-  useEffect(() => {
-    console.log(tracks);
-  }, [tracks]);
   const onSynthesizedPlay = () => {
     synthesized.current.play();
     setIsPlaying(true);
@@ -100,23 +44,18 @@ const Editor = ({ userObj }) => {
     synthesized.current.pause();
     setIsPlaying(false);
   };
-  const onSessionPlay = (index) => {
+  const onSessionPlay = () => {
     sessionsRef.current.forEach((element) => {
       element.play();
     });
-    // tracks[index].play();
-    console.log(index);
   };
-  const onSessionPause = (index) => {
+  const onSessionPause = () => {
     sessionsRef.current.forEach((element) => {
       element.pause();
     });
-    // tracks[index].pause();
-    console.log(index);
   };
   const onSessionMute = (index) => {
-    console.log(index, "mute");
-    tracks[index].toggleMute();
+    sessionsRef.current[index].muted = !sessionsRef.current[index].muted;
   };
   const onRecordPlay = () => {
     synthesized.current.currentTime = 0;
@@ -176,12 +115,12 @@ const Editor = ({ userObj }) => {
                 <FontAwesomeIcon
                   icon="play"
                   className="playbar-play"
-                  onClick={() => onSessionPlay(index)}
+                  onClick={onSessionPlay}
                 />
                 <FontAwesomeIcon
                   icon="pause"
                   className="playbar-pause"
-                  onClick={() => onSessionPause(index)}
+                  onClick={onSessionPause}
                 />
                 <FontAwesomeIcon
                   icon="volume-xmark"
